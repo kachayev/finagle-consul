c926b125d7bbb66101611afe5fd98fa08e41bdf1
Dmitry Galinsky <dima.exe@gmail.com>
2015-09-12 21:50:27 +0300

    remove obsolete Announcer and Resolver

--- src/test/scala/com/twitter/finagle/consul/ConsulQuerySpec.scala
+++ src/test/scala/com/twitter/finagle/consul/ConsulQuerySpec.scala
@@ -5,11 +5,11 @@ import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 class ConsulQuerySpec extends WordSpecLike with Matchers with BeforeAndAfterAll {
 
   "parse values" in {
-    ConsulQuery.decodeString("/name?dc=DC&ttl=10&tag=prod&tag=trace") match {
+    ConsulQuery.decodeString("/name?dc=DC&ttl=45&tag=prod&tag=trace") match {
       case Some(ConsulQuery(name, ttl, tags, dc)) =>
         assert(name          == "name")
-        assert(ttl.toString  == "Some(10.seconds)")
-        assert(tags          == Set("prod", "trace", "finagle"))
+        assert(ttl.toString  == "45.seconds")
+        assert(tags          == Set("prod", "trace"))
         assert(dc            == Some("DC"))
     }
   }
@@ -18,8 +18,8 @@ class ConsulQuerySpec extends WordSpecLike with Matchers with BeforeAndAfterAll 
     ConsulQuery.decodeString("") match {
       case Some(ConsulQuery(name, ttl, tags, dc)) =>
         assert(name          == "")
-        assert(ttl           == None)
-        assert(tags          == Set("finagle"))
+        assert(ttl.toString  == "10.seconds")
+        assert(tags          == Set())
         assert(dc            == None)
     }
   }
